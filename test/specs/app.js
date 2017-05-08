9fb0274eac352bd949ea11686ec89e64d7ef61f3
hrkd <hiroki@hrkd.net>
2017-05-08 18:39:10 +0900

    update

--- test/specs/app.js
+++ test/specs/app.js
@@ -6,6 +6,9 @@ module.exports = {
       .url(browser.launchUrl)
       .resizeWindow(1000, 800)
       .pause(1000)
+      .setValue('input[type=text],input[type=search]', 'nightwatch')
+      .submitForm('form')
+      .pause(2000)
       .saveScreenshot('./test/screenshots/'+browser.options.desiredCapabilities.browserName+'.png')
       .end();
   }
